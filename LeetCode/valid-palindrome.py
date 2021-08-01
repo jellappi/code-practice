67bfba456bd2bad45ebd19aa8d9000a29ef548e1
jellappi <jellappi324@naver.com>
2021-08-01 23:57:26 +0900

    add regex and slicing

--- LeetCode/valid-palindrome.py
+++ LeetCode/valid-palindrome.py
@@ -1,5 +1,20 @@
 https://leetcode.com/problems/valid-palindrome
 
+#solution 1 (40ms)
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        s = s.lower()
+        s = re.sub(r'[^a-z0-9]', '', s)
+        
+        if s == s[::-1]:
+            return True
+        else:
+            return False
+
+#solution 2 (52ms)
 class Solution:
     def isPalindrome(self, s: str) -> bool:
 
