9bc2d7d8fd917ac01bd601598e3b41956b272fe8
jellappi <jellappi324@naver.com>
2021-08-11 23:59:16 +0900

    first commit

---
+++ LeetCode/reconstruct-itinerary.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/reconstruct-itinerary/
+
+from collections import defaultdict
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        
+        graph = defaultdict(list)
+        
+        for a, b in sorted(tickets):
+            graph[a].append(b)
+            
+        route, stack = [], ['JFK']
+        while stack:
+            while graph[stack[-1]]:
+                stack.append(graph[stack[-1]].pop(0))
+            route.append(stack.pop())
+        
+        return route[::-1]
