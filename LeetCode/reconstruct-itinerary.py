aeb825d3484540eb39a05bc1a316c0e78bfe18e8
jellappi <jellappi324@naver.com>
2021-08-12 00:29:52 +0900

    add recursive solution

--- LeetCode/reconstruct-itinerary.py
+++ LeetCode/reconstruct-itinerary.py
@@ -2,6 +2,7 @@
 
 from collections import defaultdict
 
+# loop
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         
@@ -15,5 +16,24 @@ class Solution:
             while graph[stack[-1]]:
                 stack.append(graph[stack[-1]].pop(0))
             route.append(stack.pop())
+        
+        return route[::-1]
+
+# recursive
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        
+        graph = defaultdict(list)
+
+        for a, b in sorted(tickets, reverse=True):
+            graph[a].append(b)
+
+        route = []
+        def dfs(a):
+            while graph[a]:
+                dfs(graph[a].pop())
+            route.append(a)
+
+        dfs('JFK')
         
         return route[::-1]
